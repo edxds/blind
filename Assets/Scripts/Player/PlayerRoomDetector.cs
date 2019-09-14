8cf34b87e41ae8e94bda6efe4eac60b1cf1c9d5c
Eduardo Souza <edux404@gmail.com>
2019-09-14 16:23:16 -0300

    Add Room detection system

---
+++ Assets/Scripts/Player/PlayerRoomDetector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayerRoomDetector : MonoBehaviour {
+    private IInputProvider _inputProvider;
+
+    [SerializeField] 
+    private Transform _checkFrom;
+    private Room _currentRoom;
+
+    [Inject]
+    private void Init(IInputProvider inputProvider) {
+        _inputProvider = inputProvider;
+    }
+
+    private void Update() {
+        if (_inputProvider.ProvidePrimaryActionInput())
+            CheckIfIsInRoom();
+    }
+
+    private void CheckIfIsInRoom() {
+        var didHit = Physics.Raycast(
+            _checkFrom.position,
+            Vector3.down,
+            out var hit,
+            1
+        );
+        
+        if (!didHit)
+            return;
+
+        var room = hit.collider.GetComponent<Room>();
+        _currentRoom = room;
+    }
+}
