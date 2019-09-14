20826440a01b041f8e414778985bf31c80e839b2
Eduardo Souza <edux404@gmail.com>
2019-09-14 10:54:01 -0300

    Add AudioRingEmitterController component

---
+++ Assets/Scripts/Gameplay/AudioRingEmitterController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRingEmitterController : MonoBehaviour {
+    [SerializeField] 
+    private SoundRingEmitter _soundRingEmitter;
+    
+    public float interval;
+
+    private void Awake() {
+        if (_soundRingEmitter == null)
+            _soundRingEmitter = GetComponent<SoundRingEmitter>();
+
+        _soundRingEmitter.emissionTimeout = 0;
+    }
+
+    private void Start() {
+        StartCoroutine(StartEmittingSoundRings());
+    }
+
+    private IEnumerator StartEmittingSoundRings() {
+        while (true) {
+            yield return new WaitForSeconds(interval);
+                
+            _soundRingEmitter.EmitSoundRing();
+        }
+    }
+}
