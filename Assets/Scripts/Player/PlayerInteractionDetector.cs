a309e59074f3c66fe62b5294c18c8fbb9f469df6
Eduardo Souza <edux404@gmail.com>
2019-09-14 20:58:01 -0300

    Add PlayerInteractionDetector MonoBehaviour

---
+++ Assets/Scripts/Player/PlayerInteractionDetector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class PlayerInteractionDetector : MonoBehaviour {
+    private readonly Subject<Interactable> _currentInteractable = new Subject<Interactable>();
+
+    public IObservable<Interactable> CurrentInteractable => _currentInteractable;
+
+    private void OnTriggerEnter(Collider collider) {
+        var interactable = collider.GetComponent<Interactable>();
+        _currentInteractable.OnNext(interactable);
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        _currentInteractable.OnNext(null);
+    }
+}
