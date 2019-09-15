e3da5f496adbfbc06307d45f693a637aae2a0e11
Eduardo Souza <edux404@gmail.com>
2019-09-15 10:57:56 -0300

    Add Goal MonoBehaviour

---
+++ Assets/Scripts/Gameplay/Goal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+    private readonly Subject<Unit> _onFinish = new Subject<Unit>();
+
+    public IObservable<Unit> OnFinish => _onFinish;
+    
+    public void Finish() {
+        _onFinish.OnNext(Unit.Default);
+    }
+}
