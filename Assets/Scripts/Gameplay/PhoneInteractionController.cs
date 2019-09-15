203d586dfe65b30be4864d08a335a80b73a88335
Eduardo Souza <edux404@gmail.com>
2019-09-15 11:03:06 -0300

    Add initial PhoneInteractionController implementation

---
+++ Assets/Scripts/Gameplay/PhoneInteractionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class PhoneInteractionController : MonoBehaviour {
+    [SerializeField] private Interactable _interactable;
+    [SerializeField] private Goal _goal;
+
+    public float waitTime;
+    
+    private void Awake() {
+        if (_interactable == null)
+            _interactable = GetComponent<Interactable>();
+
+        if (_goal)
+            _goal = GetComponent<Goal>();
+    }
+
+    private void Start() {
+        _interactable.OnInteraction
+            .Do(
+                _ => { StartCoroutine(OnInteraction()); }
+            )
+            .Subscribe()
+            .AddTo(this);
+    }
+
+    private IEnumerator OnInteraction() {
+        // Do whatever...
+        
+        yield return new WaitForSeconds(waitTime);
+        _goal.Finish();
+    }
+}
