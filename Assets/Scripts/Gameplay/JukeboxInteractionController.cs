146a557550fe02ee094aa85f5ec82a69d4ab8775
Eduardo Souza <edux404@gmail.com>
2019-09-14 21:12:26 -0300

    Add JukeboxInteractionController MonoBehaviour

---
+++ Assets/Scripts/Gameplay/JukeboxInteractionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class JukeboxInteractionController : MonoBehaviour {
+    [SerializeField]
+    private Interactable _interactable;
+    [SerializeField]
+    private AudioSource _audioSource;
+    
+    private void Awake() {
+        if (_interactable == null)
+            _interactable = GetComponent<Interactable>();
+
+        if (_audioSource == null)
+            _audioSource = GetComponentInChildren<AudioSource>();
+    }
+
+    private void Start() {
+        UpdateInteractableTitle();
+
+        _interactable.OnInteraction
+            .Do(_ => { OnInteraction(); })
+            .Subscribe()
+            .AddTo(this);
+    }
+
+    private void UpdateInteractableTitle() {
+        _interactable.actionTitle = _audioSource.isPlaying
+            ? "Pausar a Música"
+            : "Tocar a Música";
+    }
+
+    private void OnInteraction() {
+        if (_audioSource.isPlaying)
+            _audioSource.Pause();
+        else 
+            _audioSource.UnPause();
+        
+        UpdateInteractableTitle();
+    }
+}
