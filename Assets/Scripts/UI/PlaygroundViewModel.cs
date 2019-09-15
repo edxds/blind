4dd122d82e05030fe7bf402f29634465f3af2849
Eduardo Souza <edux404@gmail.com>
2019-09-15 12:25:29 -0300

    Add initial PlaygroundViewModel implementation

---
+++ Assets/Scripts/UI/PlaygroundViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using Observer = UniRx.Observer;
+
+public class PlaygroundViewModel : MonoBehaviour {
+    [SerializeField] private PlayerRoomDetector _roomDetector;
+    [SerializeField] private PlayerInteractionDetector _interactionDetector;
+    [SerializeField] private Goal _mainGoal;
+
+    public string keycapSpriteName;
+    
+    public IObservable<string> CurrentRoomUpperTitle { get; private set; }
+    public IObservable<string> CurrentRoomTitle { get; private set; }
+    public IObservable<string> CurrentInteractionUpperTitle { get; private set; }
+    public IObservable<string> CurrentInteractionTitle { get; private set; }
+    public IObservable<Unit> OnGoalFinish { get; private set; }
+    
+    private void Awake() {
+        CurrentRoomUpperTitle = _roomDetector.Room
+            .Select(
+                room => room == null
+                        ? null
+                        : $"Você está {room.preposition}"
+            );
+        
+        CurrentRoomTitle = _roomDetector.Room
+            .Select(
+                room => room == null 
+                    ? null 
+                    : $"{room.title}"
+            );
+
+        CurrentInteractionUpperTitle = _interactionDetector.CurrentInteractable
+            .Select(
+                interactable => interactable == null 
+                    ? null 
+                    : $"Pressione <sprite name={keycapSpriteName}> para"
+            );
+
+        CurrentInteractionTitle = _interactionDetector.CurrentInteractable
+            .Select(
+                interactable => interactable == null
+                    ? null
+                    : interactable.actionTitle
+            );
+
+        OnGoalFinish = _mainGoal.OnFinish;
+    }
+
+    private void OnRoomChange(Room room) {
+        if (room == null)
+            return;
+                    
+        Debug.Log($"Você está {room.preposition} {room.title}");
+    }
+
+    private void OnInteractableChange(Interactable interactable) {
+        if (interactable == null)
+            return;
+        
+        Debug.Log($"Pressione E para {interactable.actionTitle}");
+    }
+
+    private void OnGoalFinished(Unit _) {
+        Debug.Log("Goal finished!");
+    }
+}
